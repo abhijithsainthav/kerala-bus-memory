bae2d1fdd8504f8e08cad88cfbbf8a454d51cc8a
abhijithsainthav <abhijithswathy93@gmail.com>
2025-07-11 23:02:29 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,29 +1,4 @@
-import { useState } from "react";
-
-const sounds = [
-  { id: "bus", label: "Bus", icon: "🚍", src: "/media/bus.mp3" },
-  { id: "crowd", label: "Crowd", icon: "👥", src: "/media/crowd.mp3" },
-  { id: "rain", label: "Rain", icon: "🌧️", src: "/media/rain.mp3" },
-  { id: "song", label: "Song", icon: "🎵", src: "/media/song.mp3" },
-];
-
-function App() {
-  const [playing, setPlaying] = useState({});
-  const refs = sounds.reduce((o, s) => ({ ...o, [s.id]: new Audio(s.src) }), {});
-
-  const toggle = (id) => {
-    const audio = refs[id];
-    if (playing[id]) {
-      audio.pause();
-      audio.currentTime = 0;
-    } else {
-      audio.loop = true;
-      audio.play();
-    }
-    setPlaying({ ...playing, [id]: !playing[id] });
-  };
-
- import "./style.css";
+import "./style.css";
 
 function App() {
   return (
@@ -37,17 +12,6 @@ function App() {
           <button><span>👥</span> Crowd</button>
           <button><span>🌧️</span> Rain</button>
           <button><span>🎵</span> Song</button>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default App;
-              <span>{s.icon}</span>
-              {s.label}
-            </button>
-          ))}
         </div>
       </div>
     </div>
